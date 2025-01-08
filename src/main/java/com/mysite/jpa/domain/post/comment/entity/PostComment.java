02dcbf8547cc7ba1d9964c41829c59fa2f3cc50b
baekminjin <bhs3392@naver.com>
2025-01-08 11:59:49 +0900

    Comment

---
+++ src/main/java/com/mysite/jpa/domain/post/comment/entity/PostComment.java
@@ -0,0 +1,41 @@
+package com.mysite.jpa.domain.post.comment.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class PostComment {
+	@Id
+	@GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
+	@Setter(AccessLevel.PRIVATE) //JPA에서 해주기 때문에 외부에서 사용하지말라는 뜻
+	private Long id;
+
+	@CreatedDate
+	@Setter(AccessLevel.PRIVATE)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Setter(AccessLevel.PRIVATE)
+	private LocalDateTime modifiedAt;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private boolean blind;
+
+	private long postId;
+}
--- src/main/java/com/mysite/jpa/global/initData/BaseinitDate.java
+++ src/main/java/com/mysite/jpa/global/initData/BaseinitDate.java
@@ -1,25 +1,22 @@
 package com.mysite.jpa.global.initData;
 
+import com.mysite.jpa.domain.post.comment.entity.PostComment;
+import com.mysite.jpa.domain.post.comment.service.PostCommentService;
 import com.mysite.jpa.domain.post.post.entity.Post;
 import com.mysite.jpa.domain.post.post.service.PostService;
-
-import com.mysite.jpa.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.transaction.annotation.Transactional;
 
 //초기 설정
 @Configuration
 @RequiredArgsConstructor
 public class BaseinitDate {
 	private final PostService postService;
+	private final PostCommentService postCommentService;
 
 	@Bean
-	@Order(1)
 	public ApplicationRunner baseInitData1ApplicationRunner() {
 		return args -> {
 			//0보다 크면 리턴
@@ -29,18 +26,16 @@ public class BaseinitDate {
 			Post post1 = postService.write("title1", "content1");
 			Post post2 = postService.write("title2", "content2");
 			Post post3 = postService.write("title3", "content3");
+
+			// 1번글에 대한 댓글 1 생성
+			PostComment postComment1 =  postCommentService.write(post1.getId(), "comment1");
+			// 1번글에 대한 댓글 2 생성
+			PostComment postComment2 = postCommentService.write(post1.getId(), "comment2");
+			// 2번글에 대한 댓글 3 생성
+			PostComment postComment3 = postCommentService.write(post2.getId(), "comment3");
+
 		};
 	}
 
-	//더 늦게 만들어지는 빈 / findById -> 인자 없는 생성자 필요 (ID가 null이기 때문에)
-	@Bean
-	@Order(2)
-	public ApplicationRunner baseInitData2ApplicationRunner() {
-		return args ->  {
-			Post post4 = postService.write("title3", "content4");
-            //한 번 매겨진 번호는 재사용하지 않는다.
-			postService.delete(post4);
-		};
-	}
 }
 
